bdcde3a0c51a5cebc1a0b335cb5b6aebeaa2bc34
sidmal11 <39958179+sidmal11@users.noreply.github.com>
2021-04-06 17:36:58 -0400

    please 4

--- backend/app.js
+++ backend/app.js
@@ -32,10 +32,10 @@ app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, "../dist/datis-ui/index.html"));
 });
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
