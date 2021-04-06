132933113b465ef7a484061cc6ca8f50a5209a7a
sidmal11 <39958179+sidmal11@users.noreply.github.com>
2021-04-06 18:54:37 -0400

    please 6

--- backend/app.js
+++ backend/app.js
@@ -30,8 +30,9 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept,Authorization"
+    "Origin, X-Requested-With,  Accept,Authorization"
   );
+  res.setHeader("Content-Type", "application/json");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PATCH,PUT, DELETE, OPTIONS"
